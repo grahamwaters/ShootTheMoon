9ea499ec17b43ccd18e6ad4bb99b4d7270e08a38
Graham Waters <graham.waters.business@gmail.com>
2022-06-08 14:39:00 -0500

    basic template additions
    see files

---
+++ src/main.py
@@ -0,0 +1,54 @@
+'''
+Placeholder Doc String
+'''
+
+import typing
+
+#
+# Demo imports from module and package
+#
+from module_example import demo_variable_imported_from_module as x
+from package_example import demo_variable_imported_from_package as y
+
+print("\n>>>" + x)
+print("\n>>>" + y)
+
+#
+# Demo usage of python typing system
+# Try changing types to confirm VSCode mypy is correctly integrated
+#
+example_float: float = 3.14
+example_int: int = 1
+example_str: str = "Wow!"
+
+example_list: typing.List[typing.Union[float, int, str]] = [
+    example_float,
+    example_int,
+    example_str
+]
+
+for i in example_list:
+    print("\n-->"+str(i)+"  <-> "+str(type(i)))
+
+# Demo class
+
+
+class Pet:
+    '''Docstring'''
+
+    def __init__(self, name: str, animal: str):
+        '''Docstring'''
+        self.name = name
+        self.animal = animal
+
+    def who_am_i(self: "Pet") -> None:
+        '''Docstring'''
+        print("\n"+self.name+" is a "+self.animal+"\n\n")
+
+
+pet1: Pet = Pet("Scooby", "Dog")
+pet1.who_am_i()
+
+pets: typing.List[Pet] = [pet1, Pet("Garfield", "Cat")]
+names: typing.List[str] = [item.name for item in pets]
+print(names)
---
+++ src/module_example.py
@@ -0,0 +1,5 @@
+'''
+Doc string
+'''
+
+demo_variable_imported_from_module: str = "Hello! I come from the module 'module_example.py'"
